2064e8e8a916e922dea73441877d9e7ead3442a8
Tom Conerly <tconerly@fb.com>
2015-09-13 19:32:52 -0700

    add cmake

--- play_main.cpp
+++ play_main.cpp
@@ -1,13 +1,21 @@
+#include <gflags/gflags.h>
+
 #include "cfr.h"
 #include "play.h"
 
 using namespace std;
 
+DEFINE_string(file, "", "The .game file to play");
+
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    return 1;
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  if (FLAGS_file == "") {
+    cout << "--file is required" << endl;
+    exit(-1);
   }
-  auto root = readFromFile(argv[1]);
+
+  auto root = readFromFile(FLAGS_file);
   cout << "value: " << root->computeValue(Player::FIRST) << " "
        << root->computeValue(Player::SECOND) << endl;
 
